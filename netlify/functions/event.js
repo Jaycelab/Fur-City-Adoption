9a7cb0d3b68c44eed3c6eb31f1c2b9f516c7886a
jaycelab <jayxalvarado@gmail.com>
2024-04-04 03:31:08 -0700

    set up mongodb and env

--- netlify/functions/event.js
+++ netlify/functions/event.js
@@ -1,14 +1,17 @@
-const handler = async () => {
-  const pets = [
-    { name: "Meowsalot", species: "cat" },
-    { name: "Barksalot", species: "dog" },
-  ];
+const {MongoClient} = require("mongodb")
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(pets),
-  };
+const handler = async () => {
+    const client = new MongoClient(process.env.CONNECTIONSTRING)
+    await client.connect()
+
+    const pets = await client.db().collection("pets").find().toArray()
+    client.close()
+
+    return {
+        statusCode: 200,
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(pets)
+    };
 };
 
-module.exports = { handler };
+module.exports = {handler};
