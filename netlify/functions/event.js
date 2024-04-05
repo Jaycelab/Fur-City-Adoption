acb92e0684f3fc7fa6dd933e6a52454872befd57
jaycelab <jayxalvarado@gmail.com>
2024-04-04 18:35:56 -0700

    reorganized folders and files

--- netlify/functions/event.js
+++ netlify/functions/event.js
@@ -1,17 +1,20 @@
-const {MongoClient} = require("mongodb")
+const { MongoClient } = require("mongodb");
 
 const handler = async () => {
-    const client = new MongoClient(process.env.CONNECTIONSTRING)
-    await client.connect()
+  const client = new MongoClient(process.env.CONNECTIONSTRING);
+  await client.connect();
 
-    const pets = await client.db().collection("pets").find().toArray()
-    client.close()
+  const pets = await client.db().collection("pets").find().toArray();
+  client.close();
 
-    return {
-        statusCode: 200,
-        headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
-        body: JSON.stringify(pets)
-    };
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(pets),
+  };
 };
 
-module.exports = {handler};
+module.exports = { handler };
