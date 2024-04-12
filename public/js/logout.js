2f075bb0288d4c2cbfe15a567d01bff343ffd71f
jaycelab <jayxalvarado@gmail.com>
2024-04-12 03:35:48 -0700

    fixed fetch url

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,5 @@
 document.querySelector("#logout").addEventListener("click", async function () {
-  const ourPromise = await fetch("/netlify/functions/logout");
+  const ourPromise = await fetch("/.netlify/functions/logout");
 
   window.location = "/";
 });
