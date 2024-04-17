f5f4457f25cd42f3bea5ceb1a46c19b1d5c465be
jaycelab <jayxalvarado@gmail.com>
2024-04-17 12:53:25 -0700

    worked on data filtering and sanitization

--- netlify/functions/addPet.js
+++ netlify/functions/addPet.js
@@ -1,16 +1,48 @@
+const sanitizeHtml = require("sanitize-html");
 const getClient = require("../../lib/getClient");
 const isAdmin = require("../../lib/isAdmin");
+
+//sanitize HTML func
+function cleanUp(san) {
+  //data to sanitize, config object
+  return sanitizeHtml(san, {
+    //nothing allowed using [] and {}
+    allowedTags: [],
+    allowedAttributes: {},
+  });
+}
 
 //parsing data into json object
 const handler = async (event) => {
   const body = JSON.parse(event.body);
   //logs json parse
-  console.log(body);
+
+  //validation for default object property
+  let pet = {
+    name: cleanUp(body.name),
+    species: cleanUp(body.species),
+    description: cleanUp(body.description),
+    //defaults to fallback current year if invalid. no sanitize needed
+    birthYear: new Date().getFullYear(),
+  };
+
+  //validation birth year between 3 & 4 digits
+  if (body.birthYear > 999 && body.birthYear < 9999) {
+    pet.birthYear = body.birthYear;
+  }
+
+  //validates species , fallback to dog
+  if (pet.species != "cat" && pet.species != "dog") {
+    pet.species = "dog";
+  }
+
   if (isAdmin(event)) {
     //saving into database
     const client = await getClient();
-    //accessing client pet database
-    await client.db().collection("pets").insertOne(body);
+    //accessing client pet database passing pet validation as param
+    await client.db().collection("pets").insertOne(pet);
+    client.close();
+
     return {
       statusCode: 200,
       header: { "Content-Type": "application/json" },
